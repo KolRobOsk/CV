c00dff1319b741e90f538c513edd23d0d308b30d
Robert <robertoskarkolke@gmail.com>
2026-07-30 10:59:32 +0200

    Dodany plik z wymaganiami oraz modyfikacja .gitignore

--- Program.py
+++ Program.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
-"""
+"""pip 
 Created on Tue Jul 28 10:39:56 2026
 
 @author: Robert
 """
+import wx
 
-print('Program Pierwszy\nLiczenie prawdopodbieństwa wyników rzutów kością')
+app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
+frame = wx.Frame(None, wx.ID_ANY, "Kalkulator") # A Frame is a top-level window.
+frame.Show(True)     # Show the frame.spyd
+app.MainLoop()
