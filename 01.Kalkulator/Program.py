e5ea7c26c99ed39d14d4f4cc216628bd5061bbb7
Robert <robertoskarkolke@gmail.com>
2026-07-30 11:29:46 +0200

    Czyszczenie kodu z niepotrzebnych komentarzy

--- 01.Kalkulator/Program.py
+++ 01.Kalkulator/Program.py
@@ -5,8 +5,7 @@ Created on Tue Jul 28 10:39:56 2026
 @author: Robert
 """
 import wx
-
-app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
-frame = wx.Frame(None, wx.ID_ANY, "Kalkulator") # A Frame is a top-level window.
-frame.Show(True)     # Show the frame.spyd
+app = wx.App(False) 
+frame = wx.Frame(None, wx.ID_ANY, "Kalkulator") 
+frame.Show(True)     
 app.MainLoop()
