055502f3755cbba8ddcc59c4b77769255e41f1c5
Robert <robertoskarkolke@gmail.com>
2026-09-15 11:49:58 +0200

    Dodany pierwszy model

--- 02.Forum/Forum/Forum/urls.py
+++ 02.Forum/Forum/Forum/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from django.shortcuts import redirect
 
 urlpatterns = [
     path("home/", include("homepage.urls")),
--- 02.Forum/Forum/homepage/models.py
+++ 02.Forum/Forum/homepage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    #Forum posts model, including index, label (max 64 characters), contents and author
+    index = models.IntegerField()
+    label = models.CharField(max_length=64)
+    contents = models.CharField()
+    author = models.CharField()
+    
